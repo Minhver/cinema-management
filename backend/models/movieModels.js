98b65074235ea628adda88fce946b2c9e1dd1de0
quangminh17 <135730102+Minhver@users.noreply.github.com>
2026-06-04 00:26:23 +0700

    Update movieModels.js

--- backend/models/movieModels.js
+++ backend/models/movieModels.js
@@ -1,1 +1,16 @@
+const db = require('../config/db');
 
+const Movie = {
+    // Lấy tất cả phim đang chiếu
+    getAll: async () => {
+        const [rows] = await db.execute('SELECT * FROM movies');
+        return rows;
+    },
+    // Thêm phim mới (dành cho Admin)
+    create: async (data) => {
+        const sql = 'INSERT INTO movies (title, duration, genre, image) VALUES (?, ?, ?, ?)';
+        return await db.execute(sql, [data.title, data.duration, data.genre, data.image]);
+    }
+};
+
+module.exports = Movie;
