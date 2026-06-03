e27e663642b0088c740031a4d5467c636dc60516
quangminh17 <135730102+Minhver@users.noreply.github.com>
2026-06-04 00:13:25 +0700

    Update sever.js

--- backend/sever.js
+++ backend/sever.js
@@ -1,1 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 
+const movieRoutes = require('./routes/movieRoutes');
+
+const app = express();
+
+// Middleware
+app.use(cors()); // Cho phép Frontend truy cập
+app.use(express.json()); // Đọc dữ liệu JSON gửi lên
+
+// Routes
+app.use('/api/movies', movieRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
