c4bfd3f1a774c61655da23cdf9a8b31ed27dcd45
quangminh17 <135730102+Minhver@users.noreply.github.com>
2026-06-04 00:22:11 +0700

    Update movieController.js

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -1,1 +1,10 @@
+const Movie = require('../models/movieModel');
 
+exports.getMovies = async (req, res) => {
+    try {
+        const movies = await Movie.getAll();
+        res.status(200).json(movies); // Trả về danh sách phim dạng JSON
+    } catch (error) {
+        res.status(500).json({ message: "Lỗi lấy danh sách phim", error });
+    }
+};
