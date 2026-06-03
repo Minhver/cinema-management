23e96a1ece66226e89a0017ed4230dd0f34b37c4
quangminh17 <135730102+Minhver@users.noreply.github.com>
2026-06-04 00:20:42 +0700

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,15 @@
+const mysql = require('mysql2');
+require('dotenv').config();
 
+// Tạo kết nối dạng Pool để tối ưu hiệu suất
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10
+});
+
+module.exports = pool.promise(); 
+// Dùng promise để dùng được async/await
